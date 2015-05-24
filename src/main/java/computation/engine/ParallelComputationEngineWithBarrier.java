28ca97a1186f317973f176dd98533c54ba9189a7
V2 <vivek.v78@gmail.com>
2015-05-25 00:13:58 +0100

    Added Barrier

--- src/main/java/computation/engine/ParallelComputationEngineWithBarrier.java
+++ src/main/java/computation/engine/ParallelComputationEngineWithBarrier.java
@@ -1,8 +1,11 @@
 package computation.engine;
 
 import computation.Calculator;
+import computation.PairOfObjects;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 /**
  * Created by vivek on 22/05/15.
@@ -26,7 +29,73 @@ public class ParallelComputationEngineWithBarrier<T> extends AbstractComputation
     }
 
     @Override
-    public void compute(List<T> objects, double deltaT, int numberOfSteps) {
-        //TODO: Implement me
+    public void compute(final List<T> objects, final double deltaT, int numberOfSteps) {
+        final CountDownLatch countDownLatch = new CountDownLatch(numberOfSteps);
+        final int numberOfParallelWorkers = Runtime.getRuntime().availableProcessors();
+
+        final CyclicBarrier barrier = new CyclicBarrier(numberOfParallelWorkers,
+                // Runnable task to be executed every time the barrier is reached/
+                // Barrier is reached?
+                // All the subsets of objectpairs have been processed by each thread.
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        calculator.taskToBePerformedWhenTheBarrierIsReached(objects,deltaT);
+                        countDownLatch.countDown();
+                    }
+                });
+
+        final Runnable[] taskPerformedOnEachObjectPairs = new Runnable[numberOfParallelWorkers];
+        final List<PairOfObjects<T>> pairOfObjects = calculator.createCombinationOfObjects(objects);
+        final int numberOfObjectPairs = pairOfObjects.size();
+        int objectPairsPerThread = numberOfObjectPairs/numberOfParallelWorkers;
+
+        if (objectPairsPerThread < minNumberOfObjectPairPerThread) {
+            // If there are very few object pairs it is faster to use a serial processor.
+            //
+            throw new IllegalArgumentException("Very few particle pairs " + numberOfObjectPairs +
+            " for the number of threads on this machine ");
+
+        }
+        final int fromIndex = 0;
+        for (int i = 0; i < numberOfParallelWorkers; i++) {
+            final int toIndex = fromIndex + objectPairsPerThread;
+            final List<PairOfObjects<T>> subSetOfObjectPairs =
+                    pairOfObjects.subList(fromIndex,
+                            (toIndex > numberOfObjectPairs) ? numberOfObjectPairs: toIndex);
+
+            // Create a runnable task for a subset of object pairs
+            taskPerformedOnEachObjectPairs[i] = new Runnable() {
+                @Override
+                public void run() {
+                    // Perform the task on each of the object pairs for this subset
+                    while(countDownLatch.getCount() >0) {
+                        // Process all the pair of particles
+                        for (PairOfObjects<T> pairOfObject : subSetOfObjectPairs) {
+                            calculator.taskToBePerformedOnPairOfObjects(pairOfObject);
+                        }
+                        try {
+                            barrier.wait();
+                        } catch (InterruptedException ex) {
+                            return;
+                        }
+                    }
+                }
+            };
+        }
+
+        // Start each worker
+        for (Runnable taskPerformedOnEachObjectPair : taskPerformedOnEachObjectPairs) {
+            new Thread(taskPerformedOnEachObjectPair).start();
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+
+        }
+
     }
+
+
 }
